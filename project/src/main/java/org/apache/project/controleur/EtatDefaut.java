195c0800e50750f029c3be8561600bbab387510f
Vincent REDOUTE <Vincent REDOUTE>
2017-10-13 14:57:07 +0200

    Début implémentation basique du controleur

---
+++ project/src/main/java/org/apache/project/controleur/EtatDefaut.java
@@ -0,0 +1,9 @@
+package org.apache.project.controleur;
+
+import org.apache.project.modele.PlanDeVille;
+import org.apache.project.vue.FenetrePrincipale;
+
+public abstract class EtatDefaut implements Etat {
+
+	public void ouvrirPlanDeVille(PlanDeVille planDeVille, FenetrePrincipale fenetrePrincipale){}
+}
