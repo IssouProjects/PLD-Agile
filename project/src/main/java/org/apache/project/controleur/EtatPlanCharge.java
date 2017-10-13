2f68690f9316eadb3a620d0e29bc15f37e716401
Vincent REDOUTE <vincent.redoute@live.fr>
2017-10-13 23:32:36 +0200

    ajout EtatPlanCharge

---
+++ project/src/main/java/org/apache/project/controleur/EtatPlanCharge.java
@@ -0,0 +1,19 @@
+package org.apache.project.controleur;
+
+import java.io.IOException;
+import org.apache.project.modele.PlanDeVille;
+import org.apache.project.vue.FenetrePrincipale;
+
+public class EtatPlanCharge extends EtatDefaut {
+
+	@Override
+	public void ouvrirPlanDeVille(PlanDeVille planDeVille, FenetrePrincipale fenetrePrincipale){
+		/*try {
+			DeserialiseurXML.charger(planDeVille);
+		} catch (ParserConfigurationException 
+				| SAXException | IOException 
+				| ExceptionXML | NumberFormatException e) {
+			fenetre.afficheMessage(e.getMessage());
+		}*/
+	}
+}
