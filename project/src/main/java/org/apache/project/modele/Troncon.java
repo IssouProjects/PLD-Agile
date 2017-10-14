4e142b0ad56a17f730816793a78198e4273eb925
Quentin JODER <quentin.joder@insa-lyon.fr>
2017-10-14 18:20:01 +0200

    Classes implementées
    Intersection, PlanDeVille et Troncon sont implémentées

--- project/src/main/java/org/apache/project/modele/Troncon.java
+++ project/src/main/java/org/apache/project/modele/Troncon.java
@@ -1,5 +1,48 @@
 package org.apache.project.modele;
 
 public class Troncon {
+	
+	private int longueur;
+	private String nomRue;
+	private int idIntersectionDepart;
+	private int idIntersectionArrivee;
+	
+	public Troncon(int longueur, int idInterDepart, int idInterArrivee, String rue) {
+		this.setLongueur(longueur);
+		this.setNomRue(rue);
+		this.setIdIntersectionDepart(idInterDepart);
+		this.setIdIntersectionArrivee(idInterArrivee);
+	}
 
+	public int getLongueur() {
+		return longueur;
+	}
+
+	public void setLongueur(int longueur) {
+		this.longueur = longueur;
+	}
+
+	public String getNomRue() {
+		return nomRue;
+	}
+
+	public void setNomRue(String nomRue) {
+		this.nomRue = nomRue;
+	}
+
+	public int getIdIntersectionDepart() {
+		return idIntersectionDepart;
+	}
+
+	public void setIdIntersectionDepart(int idIntersectionDepart) {
+		this.idIntersectionDepart = idIntersectionDepart;
+	}
+
+	public int getIdIntersectionArrivee() {
+		return idIntersectionArrivee;
+	}
+
+	public void setIdIntersectionArrivee(int idIntersectionArrivee) {
+		this.idIntersectionArrivee = idIntersectionArrivee;
+	}
 }
