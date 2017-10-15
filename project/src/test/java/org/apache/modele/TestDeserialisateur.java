f2c55de706a5250e5e6ff82e8ca000be20a0e78c
Victor <vb4007@gmail.com>
2017-10-15 22:34:42 +0200

    Appel du déserialisateur dans test

---
+++ project/src/test/java/org/apache/modele/TestDeserialisateur.java
@@ -0,0 +1,31 @@
+package org.apache.modele;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.project.modele.PlanDeVille;
+import org.apache.project.xml.Deserialisateur;
+import org.apache.project.xml.ExceptionXML;
+import org.xml.sax.SAXException;
+
+import junit.framework.TestCase;
+
+public class TestDeserialisateur extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testChargerPlanDeVille() throws ParserConfigurationException, SAXException, IOException, ExceptionXML {
+		File xml = new File("src\\test\\java\\org\\apache\\modele\\planLyonPetit.xml");
+		PlanDeVille plan = new PlanDeVille();
+		Deserialisateur.chargerPlanDeVilleFichier(plan, xml);
+	}
+
+}
