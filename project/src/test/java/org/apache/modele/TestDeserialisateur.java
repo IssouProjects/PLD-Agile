6afacb4695a151a68547679aafdb19c2befe77eb
Quentin JODER <quentinjoder@gmail.com>
2017-10-16 11:45:44 +0200

    Ajout de test
    Test de fichier de DL incorrect

--- project/src/test/java/org/apache/modele/TestDeserialisateur.java
+++ project/src/test/java/org/apache/modele/TestDeserialisateur.java
@@ -15,38 +15,57 @@ import junit.framework.TestCase;
 
 public class TestDeserialisateur extends TestCase {
 
+	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
 
+	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
-	
+
 	public void testChargerPlanDeVille() throws ParserConfigurationException, SAXException, IOException, ExceptionXML {
 		File xml = new File("src/test/java/org/apache/modele/planLyonPetit.xml");
 		PlanDeVille plan = new PlanDeVille();
 		Deserialisateur.chargerPlanDeVilleFichier(plan, xml);
 	}
-	
-	public void testChargerDemandeDeLivraison() throws ParserConfigurationException, SAXException, IOException, ExceptionXML {
-		File xml =new File("src/test/java/org/apache/modele/DLmoyen5TW1.xml");
+
+	public void testChargerDemandeDeLivraison()
+			throws ParserConfigurationException, SAXException, IOException, ExceptionXML {
+		File xml = new File("src/test/java/org/apache/modele/DLmoyen5TW1.xml");
 		File planxml = new File("src/test/java/org/apache/modele/planLyonPetit.xml");
 		PlanDeVille plan = new PlanDeVille();
 		Deserialisateur.chargerPlanDeVilleFichier(plan, planxml);
-		DemandeDeLivraison demande =new DemandeDeLivraison();
+		DemandeDeLivraison demande = new DemandeDeLivraison();
 		Deserialisateur.chargerDemandeLivraisonFichier(demande, plan, xml);
 	}
-	
-	public void testChargerPlanDeVilleIncorrect() throws ParserConfigurationException, SAXException, IOException, ExceptionXML {
+
+	public void testChargerPlanDeVilleIncorrect()
+			throws ParserConfigurationException, SAXException, IOException, ExceptionXML {
 		try {
 			File xml = new File("src/test/java/org/apache/modele/planLyonPetitIncorrect.xml");
 			PlanDeVille plan = new PlanDeVille();
 			Deserialisateur.chargerPlanDeVilleFichier(plan, xml);
-	        fail("Une exception devrait etre renvoyee car ce fichier a un noeud invalide");
-	    } catch (Exception e) {
-	        assertEquals(e.getMessage(), "Document non conforme");
-	    }
+			fail("Une exception devrait etre renvoyee car ce fichier a un noeud invalide");
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "Document non conforme");
+		}
+	}
+
+	public void testChargerDemandeDeLivraisonIncorrecte()
+			throws ParserConfigurationException, SAXException, IOException, ExceptionXML {
+		try {
+			File xml = new File("src/test/java/org/apache/modele/DLmoyen5TW2.xml");
+			File planxml = new File("src/test/java/org/apache/modele/planLyonPetit.xml");
+			PlanDeVille plan = new PlanDeVille();
+			Deserialisateur.chargerPlanDeVilleFichier(plan, planxml);
+			DemandeDeLivraison demande = new DemandeDeLivraison();
+			Deserialisateur.chargerDemandeLivraisonFichier(demande, plan, xml);
+			fail("Une exception devrait etre renvoyee car ce fichier a un noeud invalide");
+		} catch (Exception e) {
+			assertEquals(e.getMessage(), "Document non conforme");
+		}
 	}
 
 }
