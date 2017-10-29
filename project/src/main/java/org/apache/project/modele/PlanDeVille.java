338e2519e9aa769698c8544553fe4f25dddf438c
Quentin JODER <quentinjoder@gmail.com>
2017-10-29 22:13:52 +0100

    debut javadoc plan

--- project/src/main/java/org/apache/project/modele/PlanDeVille.java
+++ project/src/main/java/org/apache/project/modele/PlanDeVille.java
@@ -6,42 +6,77 @@ import java.util.List;
 import java.util.Map;
 import java.util.Observable;
 
+/**
+ * La classe <tt>PlanDeVille</tt> représente un plan des rues d'une
+ * ville/agglomération, il n'a que des tronçons et des intersections.
+ */
 public class PlanDeVille extends Observable {
-	
-	private Map<Long,Intersection> intersections;
+
+	private Map<Long, Intersection> intersections;
 	private List<Troncon> troncons;
-	
+
+	/**
+	 * Construis un plan routier vide.
+	 */
 	public PlanDeVille() {
 		intersections = new HashMap<Long, Intersection>();
 		troncons = new ArrayList<Troncon>();
 	}
-	
+
+	/**
+	 * @param id
+	 * @param coordX
+	 * @param coordY
+	 */
 	public void ajouterIntersection(Long id, Long coordX, Long coordY) {
-		intersections.put(id, new Intersection(id,coordX,coordY));
+		intersections.put(id, new Intersection(id, coordX, coordY));
 	}
-	
+
+	/**
+	 * @param distance
+	 * @param numDepart
+	 * @param numArrivee
+	 * @param nomRue
+	 */
 	public void ajouterTroncon(double distance, Long numDepart, Long numArrivee, String nomRue) {
-		Troncon troncon = new Troncon(distance,intersections.get(numDepart),intersections.get(numArrivee),nomRue);
+		Troncon troncon = new Troncon(distance, intersections.get(numDepart), intersections.get(numArrivee), nomRue);
 		intersections.get(numDepart).ajouterTronconPartant(troncon);
 		troncons.add(troncon);
 	}
-	
-	public Map<Long,Intersection> getAllIntersections(){
+
+	/**
+	 * @return
+	 */
+	public Map<Long, Intersection> getAllIntersections() {
 		return intersections;
 	}
-	
+
+	/**
+	 * @param id
+	 * @return
+	 */
 	public Intersection getIntersection(Long id) {
 		return intersections.get(id);
 	}
-	
-	public List<Troncon> getAllTroncons(){
+
+	/**
+	 * @return
+	 */
+	public List<Troncon> getAllTroncons() {
 		return troncons;
 	}
-	
+
+	/**
+	 * @param id
+	 * @return
+	 */
 	public Intersection getIntersectionById(Long id) {
 		return intersections.get(id);
 	}
-	
+
+	/**
+	 * 
+	 */
 	public void clear() {
 		intersections.clear();
 		troncons.clear();
