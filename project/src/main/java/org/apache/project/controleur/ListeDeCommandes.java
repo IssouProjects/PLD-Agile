758c47dc14a2de818196f6d1e4444fc161c78419
Victor <vb4007@gmail.com>
2017-10-31 16:27:23 +0100

    Squelettes de classes pour undo-redo

---
+++ project/src/main/java/org/apache/project/controleur/ListeDeCommandes.java
@@ -0,0 +1,25 @@
+package org.apache.project.controleur;
+
+import java.util.List;
+
+public class ListeDeCommandes {
+	
+	private List<Commande> liste;
+
+	public ListeDeCommandes() {
+		
+	}
+	
+	public void ajouteCommande(Commande commande) {
+		
+	}
+	
+	public void undo() {
+		
+	}
+	
+	public void redo() {
+		
+	}
+	
+}
