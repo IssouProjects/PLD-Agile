bb99401a839b43224448d01e9c6b064689e9120b
Tim <durandtimothee@orange.fr>
2017-10-31 17:09:17 +0100

    amélioration de la popup livraison

--- project/src/main/java/org/apache/project/vue/LivraisonPopup.java
+++ project/src/main/java/org/apache/project/vue/LivraisonPopup.java
@@ -12,6 +12,8 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
@@ -27,11 +29,14 @@ public class LivraisonPopup extends VBox {
 	private Pane parentPane;
 
 	private Spinner<Integer> dureeSpinner;
+	
+	private Label heureDebLabel;
+	private Label heureFinLabel;
 	private Spinner<Integer> heureDebSpinner;
 	private Spinner<Integer> heureFinSpinner;
 	private CheckBox checkBox;
 
-	private HBox heureLayout;
+	private GridPane mainLayout;
 
 	public static final String VALIDATE = "Valider";
 	public static final String VALIDATE_ID = "validerAjoutLivraisonButton";
@@ -42,44 +47,53 @@ public class LivraisonPopup extends VBox {
 	public LivraisonPopup(Livraison livraison, Pane parent, EcouteurDeBouton edb) {
 		parentPane = parent;
 		edb.setPopup(this);
-
-		setPrefSize(300, 300);
-		setStyle("-fx-background-color: #FFFFFF;");
+		this.setMaxSize(400, 300);
+		this.setSpacing(40);
+		this.setPadding(new Insets(20,40,20,40));
+		setStyle("-fx-background-color: #FFFFFF; -fx-background-radius: 10px; -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);");
+		
+		Label title = new Label("Nouvelle livraison");
+		title.setStyle("-fx-font-weight: bold; -fx-font-size: 24;");
+		this.getChildren().add(title);
 
 		SpinnerValueFactory<Integer> dureeFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 3600, 100);
 
 		StackPane.setMargin(this, new Insets(100, 100, 100, 100));
+		
+		mainLayout = new GridPane();
+		mainLayout.setHgap(10);
+		mainLayout.setVgap(10);
 
-		HBox dureeLayout = new HBox();
-		dureeLayout.setAlignment(Pos.CENTER_LEFT);
-		Label dureeLabel = new Label("temps sur place");
+		mainLayout.setAlignment(Pos.CENTER_LEFT);
+		Label dureeLabel = new Label("Durée sur place :");
 		dureeSpinner = new Spinner<Integer>();
 		dureeSpinner.setValueFactory(dureeFactory);
-		dureeLayout.getChildren().add(dureeLabel);
-		dureeLayout.getChildren().add(dureeSpinner);
+		mainLayout.add(dureeLabel, 0, 0);
+		mainLayout.add(dureeSpinner, 1, 0);
 
 		checkBox = new CheckBox();
 		checkBox.setText("Plage horaire");
 		checkBox.setSelected(false);
 		checkBox.setAlignment(Pos.CENTER_LEFT);
+		mainLayout.add(checkBox, 0, 1, 2, 1);
 
 		SpinnerValueFactory<Integer> heureFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 24, 12);
 		SpinnerValueFactory<Integer> heureFactory2 = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 24, 12);
 
-		heureLayout = new HBox();
-		heureLayout.setAlignment(Pos.CENTER_LEFT);
-		Label heureDebLabel = new Label("Heure de début:");
+		mainLayout.setAlignment(Pos.CENTER_LEFT);
+		heureDebLabel = new Label("Heure de début:");
 		heureDebSpinner = new Spinner<Integer>();
 		heureDebSpinner.setValueFactory(heureFactory);
-		Label heureFinLabel = new Label("Heure de fin:");
+		heureFinLabel = new Label("Heure de fin:");
 		heureFinSpinner = new Spinner<Integer>();
 		heureFinSpinner.setValueFactory(heureFactory2);
-		heureLayout.getChildren().add(heureDebLabel);
-		heureLayout.getChildren().add(heureDebSpinner);
-		heureLayout.getChildren().add(heureFinLabel);
-		heureLayout.getChildren().add(heureFinSpinner);
-		heureLayout.setSpacing(10);
-		heureLayout.setDisable(true);
+		
+		mainLayout.add(heureDebLabel, 0, 2);
+		mainLayout.add(heureDebSpinner, 1, 2);
+		mainLayout.add(heureFinLabel, 0, 3);
+		mainLayout.add(heureFinSpinner, 1, 3);
+		
+		disablePlageHoraire(true);
 
 		HBox buttonLayout = new HBox();
 		boutonAnnuler = new Button(CANCEL);
@@ -91,11 +105,9 @@ public class LivraisonPopup extends VBox {
 		buttonLayout.setAlignment(Pos.CENTER_RIGHT);
 		buttonLayout.setSpacing(10);
 
-		getChildren().add(dureeLayout);
-		getChildren().add(checkBox);
-		getChildren().add(heureLayout);
+		getChildren().add(mainLayout);
 		getChildren().add(buttonLayout);
-		setSpacing(10);
+		
 
 		opaqueLayer = new Region();
 		opaqueLayer.setStyle("-fx-background-color: #00000088;");
@@ -105,6 +117,10 @@ public class LivraisonPopup extends VBox {
 		parent.getChildren().add(this);
 
 		setAlignment(Pos.CENTER);
+		
+		ColumnConstraints cc1 = new ColumnConstraints();
+		cc1.setPercentWidth(50d);
+		mainLayout.getColumnConstraints().add(cc1);
 
 		boutonValider.setOnAction(edb);
 		boutonAnnuler.setOnAction(edb);
@@ -112,9 +128,16 @@ public class LivraisonPopup extends VBox {
 		checkBox.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				heureLayout.setDisable(!checkBox.isSelected());
+				disablePlageHoraire(!checkBox.isSelected());
 			}
 		});
+	}
+	
+	public void disablePlageHoraire(boolean disable) {
+		heureDebLabel.setDisable(disable);
+		heureDebSpinner.setDisable(disable);
+		heureFinLabel.setDisable(disable);
+		heureFinSpinner.setDisable(disable);
 	}
 
 	public Integer getNewDuree() {
