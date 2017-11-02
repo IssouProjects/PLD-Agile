9102840ce230b72732faf8cb3c7325af4f030c9b
Victor <vb4007@gmail.com>
2017-11-02 12:05:34 +0100

    ListeDeCommandes implémentée

--- project/src/main/java/org/apache/project/controleur/ListeDeCommandes.java
+++ project/src/main/java/org/apache/project/controleur/ListeDeCommandes.java
@@ -1,25 +1,30 @@
 package org.apache.project.controleur;
 
-import java.util.List;
+import java.util.LinkedList;
 
 public class ListeDeCommandes {
 	
-	private List<Commande> liste;
+	private LinkedList<Commande> liste;
+	private int i;
 
 	public ListeDeCommandes() {
-		
+		liste = new LinkedList();
 	}
 	
 	public void ajouteCommande(Commande commande) {
-		
+		liste.add(commande);
 	}
 	
 	public void undo() {
-		
+		if(i>=0) {
+			liste.get(i--).undoCommande();
+		}
 	}
 	
 	public void redo() {
-		
+		if(i<liste.size()-1) {
+			liste.get(i++).doCommande();
+		}
 	}
 	
 }
