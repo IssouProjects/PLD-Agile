1361c9a0a7c2de61661a7c19022875f65aefb04b
Victor <vb4007@gmail.com>
2017-11-02 11:15:06 +0100

    Classes commande ajout/suppression ok

--- project/src/main/java/org/apache/project/controleur/CdeAjouterLivraison.java
+++ project/src/main/java/org/apache/project/controleur/CdeAjouterLivraison.java
@@ -1,27 +1,32 @@
 package org.apache.project.controleur;
 
 import org.apache.project.modele.Livraison;
+import org.apache.project.modele.PlanDeVille;
 import org.apache.project.modele.Tournee;
 
 public class CdeAjouterLivraison implements Commande {
 	
+	private PlanDeVille plan;
 	private Tournee tournee;
+	private Livraison livraisonPrecedente;
 	private Livraison livraison;
 	
-	public CdeAjouterLivraison(Tournee tournee, Livraison livraison) {
+	public CdeAjouterLivraison(PlanDeVille plan, Tournee tournee, Livraison livraison, Livraison livraisonPrecedente) {
+		this.plan = plan;
 		this.tournee = tournee;
 		this.livraison = livraison;
+		this.livraisonPrecedente = livraisonPrecedente;
 	}
 	
 	@Override
 	public void doCommande() {
-		tournee.ajouterLivraison(livraison);
+		tournee.ajouterNouvelleLivraison(plan, livraison, livraisonPrecedente);
 	}
 
 	@Override
 	public void undoCommande() {
 		int index = tournee.getLivraisonsOrdonnees().indexOf(livraison);
-		tournee.supprimerLivraison(index);
+		tournee.supprimerLivraison(plan, index);
 	}
 
 }
