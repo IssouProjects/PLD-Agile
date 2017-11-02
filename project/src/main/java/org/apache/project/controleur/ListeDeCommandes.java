c9204120c30e5a9c047efac58a874fd3c9df529f
Victor <vb4007@gmail.com>
2017-11-02 16:17:17 +0100

    Manipulation sur les indices de la liste de commandes

--- project/src/main/java/org/apache/project/controleur/ListeDeCommandes.java
+++ project/src/main/java/org/apache/project/controleur/ListeDeCommandes.java
@@ -9,21 +9,25 @@ public class ListeDeCommandes {
 
 	public ListeDeCommandes() {
 		liste = new LinkedList<Commande>();
+		i=-1;
 	}
 	
 	public void ajouteCommande(Commande commande) {
 		liste.add(commande);
+		i++;
 	}
 	
 	public void undo() {
 		if(i>=0) {
-			liste.get(i--).undoCommande();
+			liste.get(i).undoCommande();
+			i--;
 		}
 	}
 	
 	public void redo() {
 		if(i<liste.size()-1) {
-			liste.get(i++).doCommande();
+			i++;
+			liste.get(i).doCommande();
 		}
 	}
 	
