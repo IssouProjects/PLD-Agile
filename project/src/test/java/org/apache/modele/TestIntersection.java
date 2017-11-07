f5146a0fcfedc9d0d55c3b7defc303a2da6eaa8e
Victor <vb4007@gmail.com>
2017-11-07 19:56:40 +0100

    Test cas particulier Intersection.TestGetTronconParDestination

---
+++ project/src/test/java/org/apache/modele/TestIntersection.java
@@ -0,0 +1,29 @@
+package org.apache.modele;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.project.modele.DemandeDeLivraison;
+import org.apache.project.modele.PlanDeVille;
+import org.apache.project.modele.Tournee;
+import org.apache.project.xml.Deserialisateur;
+import org.apache.project.xml.ExceptionXML;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+public class TestIntersection {
+
+	@Test
+	public void TestGetTronconParDestination() throws ParserConfigurationException, SAXException, IOException, ExceptionXML {
+		File planxml = new File("src/test/java/org/apache/modele/fichiers/planLyonPetit.xml");
+		PlanDeVille plan = new PlanDeVille();
+		Deserialisateur.chargerPlanDeVilleFichier(plan, planxml);
+		
+		assertNull(plan.getAllIntersections().get(Long.valueOf("26155370")).getTronconParDestination(Long.valueOf("459797860")));
+	}
+
+}
