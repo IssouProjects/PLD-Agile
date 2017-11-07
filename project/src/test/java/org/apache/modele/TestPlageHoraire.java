6e0e44f2d112772851249025f40af24a9b0560d2
Victor <vb4007@gmail.com>
2017-11-07 18:59:11 +0100

    Test cas particuliers PlageHoraire

--- project/src/test/java/org/apache/modele/TestPlageHoraire.java
+++ project/src/test/java/org/apache/modele/TestPlageHoraire.java
@@ -2,15 +2,29 @@ package org.apache.modele;
 
 import static org.junit.Assert.*;
 
+import java.sql.Time;
+
 import org.apache.project.modele.PlageHoraire;
 import org.junit.Test;
 
 public class TestPlageHoraire {
 
+	@SuppressWarnings("deprecation")
+	@Test
+	public void TestCalculerHeureArrivee() {
+		assertEquals("01:03:19", PlageHoraire.calculerHeureArrivee(new Time(23,59,59), 200).toString());
+	}
+	
+	@Test
+	public void TestAfficherMillisecondesEnHeuresEtMinutes() {
+		assertEquals("80h 18min", PlageHoraire.afficherMillisecondesEnHeuresEtMinutes(289093981));
+	}
+	
 	@Test
 	public void TestSecondesEnLocalTime() {
 		assertEquals("03:07:14", PlageHoraire.secondesEnLocalTime(11234).toString());
 		assertEquals("20:23:29", PlageHoraire.secondesEnLocalTime(73409).toString());
+		assertEquals("04:46:39", PlageHoraire.secondesEnLocalTime(99999).toString());
 	}
 
 }
