0d422641f9881e2c4fc68ccf8323e4f363bafe08
Quentin JODER <quentinjoder@gmail.com>
2017-11-08 18:27:39 +0100

    Javadoc EcouteurDeMap

--- project/src/main/java/org/apache/project/vue/EcouteurDeMap.java
+++ project/src/main/java/org/apache/project/vue/EcouteurDeMap.java
@@ -6,35 +6,46 @@ import org.apache.project.modele.Livraison;
 import org.apache.project.modele.Troncon;
 
 /**
- *
+ * Ecouteur des interactions sur la carte (map).
  */
 public class EcouteurDeMap {
 	private Controleur controleur;
 
 	/**
+	 * Construit un nouvelle écouteur de map.
+	 * 
 	 * @param c
-	 * @param mapContainer
+	 *            contrôleur de l'application
 	 */
 	public EcouteurDeMap(Controleur c) {
 		controleur = c;
 	}
 
 	/**
+	 * Gère le clic sur une intersection.
+	 * 
 	 * @param intersection
+	 *            <tt>Intersection</tt> cliquée
 	 */
 	public void onIntersectionClicked(Intersection intersection) {
 		controleur.intersectionClicked(intersection);
 	}
 
 	/**
+	 * Gère le clic sur un tronçon.
+	 * 
 	 * @param troncon
+	 *            <tt>Troncon</tt> cliqué
 	 */
 	public void onTronconClicked(Troncon troncon) {
 		controleur.tronconClicked(troncon);
 	}
 
 	/**
+	 * Gère le clic sur une livraison.
+	 * 
 	 * @param livraison
+	 *            <tt>Livraison</tt> cliquée
 	 */
 	public void onLivraisonClicked(Livraison livraison) {
 		controleur.livraisonClicked(livraison);
